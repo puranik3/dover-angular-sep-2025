638dd9e0b5be908a08a9bb65343ca6de14881231
Prashanth Puranik <puranik@digdeeper.in>
2025-09-02 15:06:49 +0530

    Step 11: Showing list of workshops and loading, error states

--- demos/02-angular/workshops-app/src/app/workshops/workshops-list/workshops-list.ts
+++ demos/02-angular/workshops-app/src/app/workshops/workshops-list/workshops-list.ts
@@ -1,24 +1,39 @@
 import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { Workshops } from '../workshops';
+
 import IWorkshop from '../models/IWorkshop';
 
 @Component({
   selector: 'app-workshops-list',
-  imports: [],
+  imports: [
+    CommonModule,
+    NgbModule
+  ],
   templateUrl: './workshops-list.html',
   styleUrl: './workshops-list.scss'
 })
 export class WorkshopsList implements OnInit {
   workshops : IWorkshop[] = [];
+  error: Error | null = null;
+  loading: boolean = true;
 
   constructor( private w : Workshops ) {
   }
 
   ngOnInit() {
+    this.loading = true;
+
     this.w.getWorkshops().subscribe({ // pass an Observer
       next: ( workshops ) => {
         this.workshops = workshops;
+        this.loading = false;
         console.log( this.workshops );
+      },
+      error: ( err ) => {
+        this.error = err;
+        this.loading = false;
       }
     });
   }
