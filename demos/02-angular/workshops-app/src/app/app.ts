b331634de3954417228f7ae1dae078aa98d74a54
Prashanth Puranik <puranik@digdeeper.in>
2025-09-02 10:37:00 +0530

    Step 5: Add a Home component

--- demos/02-angular/workshops-app/src/app/app.ts
+++ demos/02-angular/workshops-app/src/app/app.ts
@@ -4,13 +4,15 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NgbAlertModule } from '@ng-bootstrap/ng-bootstrap';
 import { Menu } from './menu/menu';
+import { Home } from './home/home';
 
 @Component({
   selector: 'app-root',
   imports: [
     RouterOutlet,
     NgbAlertModule,
-    Menu
+    Menu,
+    Home
   ],
   templateUrl: './app.html',
   styleUrl: './app.scss'
