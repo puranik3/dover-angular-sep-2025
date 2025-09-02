1d40541cf1a5c673a6f0e16c915bfea7df9a0417
Prashanth Puranik <puranik@digdeeper.in>
2025-09-02 11:04:14 +0530

    Step 6: Set up more pages and routing

---
+++ demos/02-angular/workshops-app/src/app/workshops/workshops.routes.ts
@@ -0,0 +1,23 @@
+import { Routes } from '@angular/router';
+
+import { WorkshopsList } from './workshops-list/workshops-list';
+import { AddWorkshop } from './add-workshop/add-workshop';
+import { Favorites } from './favorites/favorites';
+
+export const routes: Routes = [
+    {
+        path: 'workshops',
+        component: WorkshopsList,
+        title: 'List of workshops'
+    },
+    {
+        path: 'workshops/add',
+        component: AddWorkshop,
+        title: 'List of workshops'
+    },
+    {
+        path: 'workshops/favorites',
+        component: Favorites,
+        title: 'Favorite workshops'
+    },
+];
