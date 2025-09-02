0348c2b7279551404c816a51cd6f7f1070d31680
Prashanth Puranik <puranik@digdeeper.in>
2025-09-02 11:27:35 +0530

    Step 7 (Partial): Update the menu to route to the pages, and highlight the active route

--- demos/02-angular/workshops-app/src/app/menu/menu.ts
+++ demos/02-angular/workshops-app/src/app/menu/menu.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-menu',
   imports: [
-    NgbModule
+    NgbModule,
+    RouterLink
   ],
   templateUrl: './menu.html',
   styleUrl: './menu.scss'
