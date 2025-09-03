3874621ca1f98d881078e22d2d8f03213c924ae1
Prashanth Puranik <puranik@digdeeper.in>
2025-09-03 16:34:04 +0530

    Step 26: Create a voting widget component and use it for voting on a session

---
+++ demos/02-angular/workshops-app/src/app/workshops/sessions.ts
@@ -0,0 +1,24 @@
+import { inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import ISession from './models/ISession';
+
+export type VoteType = 'upvote' | 'downvote';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class Sessions {
+  private http = inject(HttpClient);
+
+  getSessionsForWorkshop( workshopId : number ) {
+    return this.http.get<ISession[]>(`https://workshops-server.onrender.com/workshops/${workshopId}/sessions`);
+  }
+
+  voteForSession(sessionId: number, voteType: VoteType) {
+    return this.http.put<ISession>(
+        `https://workshops-server.onrender.com/sessions/${sessionId}/${voteType}`,
+        null // we generally pass data in put request. In this case we don't have any data
+    );
+  }
+}
