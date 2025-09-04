941a814a7882bad1961dc2be0c6e5b212386bfe0
Prashanth Puranik <puranik@digdeeper.in>
2025-09-04 10:49:04 +0530

    Step 32: Validation using template-driven forms approach

--- demos/02-angular/workshops-app/src/app/workshops/workshop-details/add-session/add-session.ts
+++ demos/02-angular/workshops-app/src/app/workshops/workshop-details/add-session/add-session.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { JsonPipe } from '@angular/common';
 
 @Component({
   selector: 'app-add-session',
-  imports: [],
+  imports: [
+    FormsModule,
+    JsonPipe
+  ],
   templateUrl: './add-session.html',
   styleUrl: './add-session.scss'
 })
 export class AddSession {
-
+  sequenceId!: string
 }
